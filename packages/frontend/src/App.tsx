0b36201c4fbca81e0eee9f364b1280fe1843e065
ookimaki <noreply@ookimaki.com>
2021-03-19 13:20:21 +0900

    checked global component

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,14 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 import { RecoilRoot } from "recoil";
-
-import { LoadingOverlay } from "./components/molecules/LoadingOverlay";
-import { MessageModal } from "./components/molecules/MessageModal";
-import { NotificationToast } from "./components/molecules/NotificationToast";
-
-import { useLoadingOverlay, useMessageModal, useNotificationToast } from "./components/utils/atoms";
-
+import { AtomsRootLoader } from "./components/utils/atoms";
 import Contract from "./pages/_chainId/_contractAddress";
 import NFT from "./pages/_chainId/_contractAddress/_tokenId";
 import CreateNFTContract from "./pages/create-nft-contract";
@@ -16,36 +9,32 @@ import Home from "./pages/index";
 import Mypage from "./pages/mypage";
 
 const App: React.FC = () => {
-  const { isLoadingOverlayDiplay } = useLoadingOverlay();
-  const { isMessageModalDisplay, messageModalProps } = useMessageModal();
-  const { isNotificationToastDisplay, notificationToastProps } = useNotificationToast();
   return (
-    <div>
+    <>
       <RecoilRoot>
-        <Router>
-          <Switch>
-            <Route path="/" exact>
-              <Home />
-            </Route>
-            <Route path="/mypage" exact>
-              <Mypage />
-            </Route>
-            <Route path="/create-nft-contract" exact>
-              <CreateNFTContract />
-            </Route>
-            <Route path="/:chainId/:nftContractAddress" exact>
-              <Contract />
-            </Route>
-            <Route path="/:chainId/:nftContractAddress/:tokenId" exact>
-              <NFT />
-            </Route>
-          </Switch>
-          {isLoadingOverlayDiplay && <LoadingOverlay />}
-          {isMessageModalDisplay && messageModalProps && <MessageModal {...messageModalProps} />}
-          {isNotificationToastDisplay && notificationToastProps && <NotificationToast {...notificationToastProps} />}
-        </Router>
+        <AtomsRootLoader>
+          <Router>
+            <Switch>
+              <Route path="/" exact>
+                <Home />
+              </Route>
+              <Route path="/mypage" exact>
+                <Mypage />
+              </Route>
+              <Route path="/create-nft-contract" exact>
+                <CreateNFTContract />
+              </Route>
+              <Route path="/:chainId/:nftContractAddress" exact>
+                <Contract />
+              </Route>
+              <Route path="/:chainId/:nftContractAddress/:tokenId" exact>
+                <NFT />
+              </Route>
+            </Switch>
+          </Router>
+        </AtomsRootLoader>
       </RecoilRoot>
-    </div>
+    </>
   );
 };
 
--- packages/frontend/src/components/utils/atoms.tsx
+++ packages/frontend/src/components/utils/atoms.tsx
@@ -1,7 +1,11 @@
+import React from "react";
 import { atom, useRecoilState } from "recoil";
 
+import { LoadingOverlay } from "../molecules/LoadingOverlay";
 import { MessageModalProps } from "../molecules/MessageModal";
+import { MessageModal } from "../molecules/MessageModal";
 import { NotificationToastProps } from "../molecules/NotificationToast";
+import { NotificationToast } from "../molecules/NotificationToast";
 
 export const loadingOverlayDisplayAtom = atom({
   key: "loadingOverlayDisplay",
@@ -63,4 +67,22 @@ export const useNotificationToast = () => {
   };
 
   return { isNotificationToastDisplay, notificationToastProps, openNotificationToast, closeNotificationToast };
+};
+
+interface AtomsRootProps {
+  children: React.ReactNode;
+}
+
+export const AtomsRootLoader: React.FC<AtomsRootProps> = ({ children }) => {
+  const { isLoadingOverlayDiplay } = useLoadingOverlay();
+  const { isMessageModalDisplay, messageModalProps } = useMessageModal();
+  const { isNotificationToastDisplay, notificationToastProps } = useNotificationToast();
+  return (
+    <>
+      {children}
+      {isLoadingOverlayDiplay && <LoadingOverlay />}
+      {isMessageModalDisplay && messageModalProps && <MessageModal {...messageModalProps} />}
+      {isNotificationToastDisplay && notificationToastProps && <NotificationToast {...notificationToastProps} />}
+    </>
+  );
 };
