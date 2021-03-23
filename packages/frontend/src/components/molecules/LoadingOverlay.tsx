7cf7ff4e596295ab97725ba7bf43d7eb57678a28
ookimaki <noreply@ookimaki.com>
2021-03-23 17:05:52 +0900

    fixed loading

--- packages/frontend/src/components/molecules/LoadingOverlay.tsx
+++ packages/frontend/src/components/molecules/LoadingOverlay.tsx
@@ -4,8 +4,7 @@ export const LoadingOverlay: React.FC = () => {
   return (
     <div className="fixed z-1 inset-0">
       <div className="flex p-4 items-center justify-center min-h-screen text-center">
-        <div className="absolute inset-0 overflow-hidden bg-black opacity-10"></div>
-        <img className="h-10 w-10" src="/img/spinner.svg" />
+        <img className="h-20 w-20 z-10" src="/img/spinner.svg" />
       </div>
     </div>
   );
