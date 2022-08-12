d6be55a7d382e7721ee052f62cd5bd794b287f15
kenta suhara <ponta.blockchain@gmail.com>
2022-08-12 12:47:14 +0900

    fix: infura ipfs api

--- packages/frontend/src/modules/ipfs.ts
+++ packages/frontend/src/modules/ipfs.ts
@@ -8,10 +8,18 @@ const ipfsOnlyHash = require("ipfs-only-hash");
 export const ipfsBaseUrl = "ipfs://";
 export const ipfsHttpsBaseUrl = "https://ipfs.io/ipfs/";
 
+// restricted with HTTP ORIGINS "https://factory.chocomint.app/"
+const projectId = "2DEjaXyZRr8OhYH1uehWvhcFZcU";
+const projectSecret = "9122faab2152b36c4e0640461ea0a827";
+const auth = "Basic " + Buffer.from(projectId + ":" + projectSecret).toString("base64");
+
 export const ipfs = createClient({
   host: "ipfs.infura.io",
   port: 5001,
   protocol: "https",
+  headers: {
+    authorization: auth,
+  },
 });
 
 export const metadataToIpfsCid = async (metadata: Metadata) => {
